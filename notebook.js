e8ebb4cd8f28e968b7ebda28966aeb889f7f8453
FayeCarter <fayecarter333@gmail.com>
2020-04-28 16:58:08 +0100

    list all returns list of note contents

--- notebook.js
+++ notebook.js
@@ -6,3 +6,11 @@ Notebook.prototype.makeNote = function(noteContents) {
     var note = new Note(noteContents)
     this.thingsToRemember.push(note)
 }
+
+Notebook.prototype.listAll = function() {
+    var notes = []
+    for (var i =0; i < this.thingsToRemember.length; i++) {
+        notes.push(this.thingsToRemember[i].content)
+    }
+    return notes
+}
--- notebookSpec.js
+++ notebookSpec.js
@@ -3,3 +3,10 @@ it('making a note in the notebook stores a note in the notebook', function () {
     notebook.makeNote('buh')
     expect(notebook.thingsToRemember[0].content).toEqual('buh')
 })
+
+
+it('notebook displays list of notes', function() {
+    var notebook = new Notebook
+    notebook.makeNote('buh')
+    expect(notebook.listAll()[0]).toEqual(['buh'][0])
+})
