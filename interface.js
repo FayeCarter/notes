69d12b6af7082c7f707124fc0bdfc49691522ba0
Alex Lewis <alexlewis374@gmail.com>
2020-04-29 15:17:46 +0100

    display feed

---
+++ interface.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var notebook = new Notebook()
+    notebook.makeNote("hello") 
+    
+    updateFeed()
+
+    function updateFeed() {
+        var noteContainer = document.getElementById('feed')
+        while (noteContainer.lastElementChild) {
+            noteContainer.removeChild(noteContainer.lastElementChild);
+        }
+        var notes = notebook.getNotes()
+        for(var i = 0; i <notes.length; i++) {
+            var div = document.createElement('div')
+            div.innerHTML = notes[i].content
+            noteContainer.appendChild(div)
+        }
+        
+    }
+})
