7455e6f839f8b75668caad672a3e4137d2884a66
Edward Phillips <59967673+Edward-Phillips@users.noreply.github.com>
2020-04-29 17:34:52 +0100

    Revert "Testing framework"

--- buildFramework.js
+++ buildFramework.js
@@ -3,17 +3,16 @@ function it(label, testFunction) {
     try { testFunction();
 }
 catch(err) {
-    var stackTrace = err.stack.split(' at ').join(' <br>     StackTrace: at ')
-    document.write(`<div style='color:red'>TEST FAILED DUE TO ERROR</div><br><br><div>${stackTrace}</div><br><br>`)
+    document.write(err.message)
 }
 };
 
 function expect(a) {
     var toEqual = function(b) {
         if (a === b) {
-            document.write('<div style="color:green">Pass</div><br><br>')
+            document.write('<div>Pass</div><br><br>')
         } else {
-            document.write(`<div style="color:red">Fail : ${a} is not equal to ${b} </div><br><br>`)
+            document.write('<div>Fail</div><br><br>')
         }
         
     }
