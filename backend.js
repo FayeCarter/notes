d6cd7c5361228a1da04ec15266e0768e3bb1c49b
Alex Lewis <alexlewis374@gmail.com>
2020-04-30 15:19:50 +0100

    backend setup, can connect to the database

---
+++ backend.js
@@ -0,0 +1,23 @@
+const { Client } = require('pg')
+
+//Then create an instance of the client class with the correct data.
+const client = new Client({
+  user: 'macbookpro',
+  host: 'localhost',
+  database: 'notes',
+  password: null,
+  port: 5432,
+})
+
+client.connect()
+
+client.query('SELECT * FROM notes', (err, res) => {
+    for (var i = 0; i < res.rows.length; i++) {
+        console.log(res.rows[i].content)
+    }
+    client.end()
+  })
+
+// client.query("INSERT INTO notes (content) VALUES ('dave');", (err, res) => {
+//     client.end()
+//   })
