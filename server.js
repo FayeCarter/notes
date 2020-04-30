81a2ef4307f3e0c2c717de643536cc38fc9666e8
Edward Phillips <edd0ddi0@gmail.com>
2020-04-30 17:26:26 +0100

    working get method added to node server

--- server.js
+++ server.js
@@ -9,32 +9,20 @@ const server = http.createServer(function(req, res) {
  res.setHeader("Access-Control-Allow-Origin", "*");
  res.writeHead(200); //status code HTTP 200 / OK
  if (req.method == "POST") {
-   console.log('something')
   var body = '';
   req.on('data', chunk => {
-      body += chunk.toString(); // convert Buffer to string
+      body += chunk.toString();
   });
   req.on('end', () => {
-      console.log(JSON.parse(body)['note_text']);
+    var receivedNote = JSON.parse(body)['note_text']
+      console.log(receivedNote);
       res.end('ok');
   });
  console.log(body);
+ } else {
+   console.log('here I would pass them all the note contents')
  };
-// if(req.params('note_text', false)) {
-//   var noteValue = req.params('note_text')
-//   console.log(noteValue)
-// } else {
-//   console.log("this would be get request response")
-// }
 
-//  let dataObj = { id: 123, name: "Bob", email: "bob@work.org" };
-//  let data = JSON.stringify(dataObj);
-//  res.end(data);
-//  let path = url.parse(req.url, true);
-
-//  res.writeHead(200,'OK', {'Content-Type':'text/plain'});
-//  res.write('This is the response\n\n');
-//  res.end('end of message to browser');
 });
 
 server.listen(1234, function() {
