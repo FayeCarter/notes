65bf36e253db4a2f8c38d3d556cd282763369b45
FayeCarter <fayecarter333@gmail.com>
2020-04-30 16:56:40 +0100

    teeny edit

--- notebook.js
+++ notebook.js
@@ -1,7 +1,7 @@
 const { Pool, Client } = require('pg')
 
 const pool = new Pool({
-    user: 'macbookpro',
+    user: 'FayeCarter',
     host: 'localhost',
     database: 'notes',
     password: null,
@@ -9,36 +9,38 @@ const pool = new Pool({
 
 })
 
+//Then create an instance of the client class with the correct data.
+const client = new Client({
+    //change depending on who's computer
+    user: 'FayeCarter',
+    host: 'localhost',
+    database: 'notes',
+    password: null,
+    port: 5432,
+  })
+  
+  
+  
 function Notebook() {
     this.thingsToRemember = []
 }
+
 
 (Notebook.prototype.makeNote = function(noteContents) {
     pool.query("INSERT INTO notes (content) VALUES ('"+ noteContents +"');", (err, res) => {
         pool.end() 
     })
-})("Alan")
+})("asffas");
 
 
-//Then create an instance of the client class with the correct data.
-const client = new Client({
-  //change depending on who's computer
-  user: 'macbookpro',
-  host: 'localhost',
-  database: 'notes',
-  password: null,
-  port: 5432,
-})
 
-client.connect()
-
-Notebook.prototype.getNotes = function() {
-
-    client.query('SELECT * FROM notes', (err, res) => {
+(Notebook.prototype.getNotes = function() {
+    client.connect()
+    client.query('SELECT * FROM notes;', (err, res) => {
         for (var i = 0; i < res.rows.length; i++) {
             console.log(res.rows[i].content)
         }
         client.end()
-    })
-}
+    });
+})();
 
